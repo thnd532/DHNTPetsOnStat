1c4329c9e293b845ab178de60c171247f4e75aca
thnd532 <80845831+thnd532@users.noreply.github.com>
2021-04-26 10:26:15 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,122 @@
+function myFunction1() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = document.getElementById("myImg1").src;
+  document.getElementById("caption").innerHTML = document.getElementById("myImg1").alt;
+
+  document.getElementsByClassName("close")[0].onclick = function() { 
+  document.getElementById("myModal").style.display = "none";
+  }
+}
+
+
+function myFunction2() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = document.getElementById("myImg2").src;
+  document.getElementById("caption").innerHTML = document.getElementById("myImg2").alt;
+
+  document.getElementsByClassName("close")[0].onclick = function() { 
+  document.getElementById("myModal").style.display = "none";
+  }
+}
+	
+function myFunction3() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = document.getElementById("myImg3").src;
+  document.getElementById("caption").innerHTML = document.getElementById("myImg3").alt;
+
+  document.getElementsByClassName("close")[0].onclick = function() { 
+  document.getElementById("myModal").style.display = "none";
+  }
+}
+
+function myFunction4() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = document.getElementById("myImg4").src;
+  document.getElementById("caption").innerHTML = document.getElementById("myImg4").alt;
+
+  document.getElementsByClassName("close")[0].onclick = function() { 
+  document.getElementById("myModal").style.display = "none";
+  }
+}
+
+function myFunction5() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = document.getElementById("myImg5").src;
+  document.getElementById("caption").innerHTML = document.getElementById("myImg5").alt;
+
+  document.getElementsByClassName("close")[0].onclick = function() { 
+  document.getElementById("myModal").style.display = "none";
+  }
+}
+
+
+function myFunction6() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = document.getElementById("myImg6").src;
+  document.getElementById("caption").innerHTML = document.getElementById("myImg6").alt;
+
+  document.getElementsByClassName("close")[0].onclick = function() { 
+  document.getElementById("myModal").style.display = "none";
+  }
+}
+	
+function myFunction7() {
+  document.getElementById("myModal2").style.display = "block";
+  document.getElementById("img02").src = document.getElementById("myImg7").src;
+  document.getElementById("caption2").innerHTML = document.getElementById("myImg7").alt;
+
+  document.getElementsByClassName("close2")[0].onclick = function() { 
+  document.getElementById("myModal2").style.display = "none";
+  }
+}
+
+function myFunction8() {
+  document.getElementById("myModal2").style.display = "block";
+  document.getElementById("img02").src = document.getElementById("myImg8").src;
+  document.getElementById("caption2").innerHTML = document.getElementById("myImg8").alt;
+
+  document.getElementsByClassName("close2")[0].onclick = function() { 
+  document.getElementById("myModal2").style.display = "none";
+  }
+}
+
+function myFunction9() {
+  document.getElementById("myModal2").style.display = "block";
+  document.getElementById("img02").src = document.getElementById("myImg9").src;
+  document.getElementById("caption2").innerHTML = document.getElementById("myImg9").alt;
+
+  document.getElementsByClassName("close2")[0].onclick = function() { 
+  document.getElementById("myModal2").style.display = "none";
+  }
+}
+
+
+function myFunction10() {
+  document.getElementById("myModal2").style.display = "block";
+  document.getElementById("img02").src = document.getElementById("myImg10").src;
+  document.getElementById("caption2").innerHTML = document.getElementById("myImg10").alt;
+
+  document.getElementsByClassName("close2")[0].onclick = function() { 
+  document.getElementById("myModal2").style.display = "none";
+  }
+}
+	
+function myFunction11() {
+  document.getElementById("myModal2").style.display = "block";
+  document.getElementById("img02").src = document.getElementById("myImg11").src;
+  document.getElementById("caption2").innerHTML = document.getElementById("myImg11").alt;
+
+  document.getElementsByClassName("close2")[0].onclick = function() { 
+  document.getElementById("myModal2").style.display = "none";
+  }
+}
+
+function myFunction12() {
+  document.getElementById("myModal2").style.display = "block";
+  document.getElementById("img02").src = document.getElementById("myImg12").src;
+  document.getElementById("caption2").innerHTML = document.getElementById("myImg12").alt;
+
+  document.getElementsByClassName("close2")[0].onclick = function() { 
+  document.getElementById("myModal2").style.display = "none";
+  }
+}
