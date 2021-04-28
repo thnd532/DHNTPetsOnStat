7adb6bc61bbf841e15560a817d449da79bc53c78
thnd532 <80845831+thnd532@users.noreply.github.com>
2021-04-28 15:35:00 +0800

    Update function.js

--- function.js
+++ function.js
@@ -120,3 +120,131 @@ function myFunction12() {
   document.getElementById("myModal2").style.display = "none";
   }
 }
+
+function v() {
+  var inpObj1 = document.getElementById("gender");
+  var inpObj2 = document.getElementById("name");
+  var inpObj3 = document.getElementById("phone");
+  var inpObj4 = document.getElementById("email");
+  var inpObj5 = document.getElementById("address");
+  var inpObj6 = document.getElementById("address2");
+  if (!inpObj1.checkValidity()) {
+    document.getElementById("error1").innerHTML = inpObj1.validationMessage;
+    document.getElementById("confirm").disabled = true;
+  } 
+  else {
+    document.getElementById("error1").innerHTML = "";
+    document.getElementById("confirm").disabled = false;
+  }
+
+  if (!inpObj2.checkValidity()) {
+    document.getElementById("error2").innerHTML = inpObj2.validationMessage;
+    document.getElementById("confirm").disabled = true;
+  }
+  else {
+    document.getElementById("error2").innerHTML = "";
+    document.getElementById("confirm").disabled = false;
+  }
+ 
+  if (!inpObj3.checkValidity()) {
+    document.getElementById("error3").innerHTML = inpObj3.validationMessage;
+    document.getElementById("confirm").disabled = true;
+  } 
+  else {
+    document.getElementById("error3").innerHTML = "";
+    document.getElementById("confirm").disabled = false;
+  }
+
+  if (!inpObj4.checkValidity()) {
+    document.getElementById("error4").innerHTML = inpObj4.validationMessage;
+    document.getElementById("confirm").disabled = true;
+  }
+  else {
+    document.getElementById("error4").innerHTML = "";
+    document.getElementById("confirm").disabled = false;
+  }
+
+    if (!inpObj5.checkValidity()) {
+    document.getElementById("error5").innerHTML = inpObj5.validationMessage;
+    document.getElementById("confirm").disabled = true;
+  }
+  else {
+    document.getElementById("error5").innerHTML = "";
+    document.getElementById("confirm").disabled = false;
+  }
+
+  if (!inpObj6.checkValidity()) {
+    document.getElementById("error6").innerHTML = inpObj6.validationMessage;
+    document.getElementById("confirm").disabled = true;
+  }
+  else {
+    document.getElementById("error6").innerHTML = "";
+    document.getElementById("confirm").disabled = false;
+  }
+} 
+
+function v2() {
+  var inpObj8 = document.getElementById("password");
+
+  if (!inpObj8.checkValidity()) {
+    document.getElementById("error8").innerHTML = inpObj8.validationMessage;
+    document.getElementById("confirm2").disabled = true;
+  }
+  else {
+    document.getElementById("error8").innerHTML = "";
+    document.getElementById("confirm2").disabled = false;
+  }
+} 
+
+function pho() {
+  
+  localStorage.setItem("iconname", document.getElementById("myFile").files[0].name);
+
+  document.getElementById("photo1").src=localStorage.getItem("iconname");
+  document.getElementById("photo2").src=localStorage.getItem("iconname");
+
+}
+
+function bg1() {
+  document.body.style.backgroundImage="";
+  document.body.style.backgroundColor="#f2f2f2";
+}
+
+function bg2() {
+  document.body.style.backgroundImage="";
+  document.body.style.backgroundColor="#ffeee6";
+}
+
+function bg3() {
+  document.body.style.backgroundImage="";
+  document.body.style.backgroundColor="#e6e6ff";
+}
+
+function bg4() {
+  document.body.style.backgroundImage="linear-gradient(to bottom right, #ffe6f3, #ff4d4d)";
+  document.body.style.backgroundColor="white";
+}
+
+function bg5() {
+  document.body.style.backgroundImage="linear-gradient(to bottom right, #f2f2f2, #9494b8)";
+  document.body.style.backgroundColor="white";
+}
+
+function bg6() {
+  document.body.style.backgroundImage="linear-gradient(180deg, #ffff80, #d9d9d9)";
+  document.body.style.backgroundColor="white";
+}
+
+function bg7() {
+  document.body.style.backgroundImage="";
+  document.body.style.backgroundColor="white";
+}
+
+function showpsw() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
